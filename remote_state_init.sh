756dd47efee7fb5ec2b527fe83c494f3b2e24331
Martin <marcin.paukstelo@devbridge.com>
2023-08-20 19:05:23 +0300

    Testing Terraform CI

--- remote_state_init.sh
+++ remote_state_init.sh
@@ -7,7 +7,5 @@ CONTAINER_NAME=statecontainer
 LOCATION=westeurope
 
 az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
-
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
-
 az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
