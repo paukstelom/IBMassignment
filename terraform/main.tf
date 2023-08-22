6e2e2cd73e839566a4d9a8a18c553f293a45414e
Martin <marcin.paukstelo@devbridge.com>
2023-08-22 08:46:39 +0300

    Testing auth

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,17 +18,17 @@ resource "azurerm_service_plan" "main" {
   tags       = var.tags
 }
 
-resource "azurerm_redis_cache" "main" {
-  name                = "${var.prefix}-REDIS"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  sku_name            = "Basic"
-  capacity            = 1
-  family              = "C"
+# resource "azurerm_redis_cache" "main" {
+#   name                = "${var.prefix}-REDIS"
+#   location            = azurerm_resource_group.main.location
+#   resource_group_name = azurerm_resource_group.main.name
+#   sku_name            = "Basic"
+#   capacity            = 1
+#   family              = "C"
 
-  depends_on = [azurerm_resource_group.main]
-  tags       = var.tags
-}
+#   depends_on = [azurerm_resource_group.main]
+#   tags       = var.tags
+# }
 
 resource "azurerm_linux_web_app" "main" {
   name                = "${var.prefix}-WEBAPP"
@@ -47,21 +47,20 @@ resource "azurerm_linux_web_app" "main" {
       docker_registry_username = var.image_registry_password
       docker_registry_password = var.image_registry_password
     }
-
   }
 
   app_settings = {
     "WEBSITES_PORT"           = "3000"
-    "REDIS_HOSTNAME" = azurerm_redis_cache.main.hostname
-    "REDIS_PORT"     = azurerm_redis_cache.main.ssl_port
-    "REDIS_ACCESS_KEY" = azurerm_redis_cache.main.primary_access_key
+    # "REDIS_HOSTNAME" = azurerm_redis_cache.main.hostname
+    # "REDIS_PORT"     = azurerm_redis_cache.main.ssl_port
+    # "REDIS_ACCESS_KEY" = azurerm_redis_cache.main.primary_access_key
 
-    # "DOCKER_REGISTRY_SERVER_URL" = var.image_registry_url
-    # "DOCKER_REGISTRY_SERVER_USERNAME" = var.image_registry_username
-    # "DOCKER_REGISTRY_SERVER_PASSWORD" = var.image_registry_password
+    "DOCKER_REGISTRY_SERVER_URL" = var.image_registry_url
+    "DOCKER_REGISTRY_SERVER_USERNAME" = var.image_registry_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD" = var.image_registry_password
   }
 
-  depends_on = [azurerm_service_plan.main, azurerm_redis_cache.main]
+  depends_on = [azurerm_service_plan.main]
   tags       = var.tags
 }
 
